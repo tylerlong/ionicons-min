c78deb6bc9eed876322785723fa8ce61e8a7f461
Tyler Long <tyler4long@gmail.com>
2015-03-17 12:25:54 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ionicons-min
+
+Ionicons minimal.
+
+
+## Installation
+
+`bower install ionicons-min`
+
+
+## For repository maintainers
+
+`fab update` to update to the latest version.
---
+++ fabfile.py
@@ -0,0 +1,13 @@
+import requests
+from fabric.api import local
+
+def update():
+    latest_version = requests.get('https://api.github.com/repos/driftyco/ionicons/releases/latest').json()['tag_name']
+    # local('rm -rf fonts/')
+    # local('rm -rf css/')
+    local('wget https://github.com/driftyco/ionicons/archive/{0}.zip'.format(latest_version))
+    local('unzip {0}.zip'.format(latest_version))
+    local('rm {0}.zip'.format(latest_version))
+    # local('rm katex/README.md')
+    # local('mv katex/* .')
+    local('rm -rf ionicons-{0}}'.format(latest_version[1:]))
