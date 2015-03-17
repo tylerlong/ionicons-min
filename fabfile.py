81314adbbb1bae3fe4d37d6c2dc760004fc62aac
Tyler Long <tyler4long@gmail.com>
2015-03-17 12:38:22 +0800

    Add css and fonts folder

--- fabfile.py
+++ fabfile.py
@@ -3,11 +3,10 @@ from fabric.api import local
 
 def update():
     latest_version = requests.get('https://api.github.com/repos/driftyco/ionicons/releases/latest').json()['tag_name']
-    # local('rm -rf fonts/')
-    # local('rm -rf css/')
     local('wget https://github.com/driftyco/ionicons/archive/{0}.zip'.format(latest_version))
-    local('unzip {0}.zip'.format(latest_version))
-    local('rm {0}.zip'.format(latest_version))
-    # local('rm katex/README.md')
-    # local('mv katex/* .')
+    local('unzip {0}.zip && rm {0}.zip'.format(latest_version))
+    local('rm -rf css/*')
+    local('rm -rf fonts/*')
+    local('cp ionicons-{0}/css/ionicons.min.css css/'.format(latest_version[1:]))
+    local('cp ionicons-{0}/fonts/* fonts/'.format(latest_version[1:]))
     local('rm -rf ionicons-{0}'.format(latest_version[1:]))
